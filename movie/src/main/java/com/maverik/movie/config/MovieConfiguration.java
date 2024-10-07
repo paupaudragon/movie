475e088de75568769353206337766dec65dcfcf7
Tingting Zhou <zhou.tingting0401@gmail.com>
2024-10-07 13:49:52 -0600

    init

---
+++ movie/src/main/java/com/maverik/movie/config/MovieConfiguration.java
@@ -0,0 +1,21 @@
+package com.maverik.movie.config;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MovieConfiguration {
+
+
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args->{
+
+
+
+        };
+
+    };
+}
